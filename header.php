095ee2a0a6ff8d2b28488913822603f0e88e6ed8
amaksimovic <amaksimovic84@gmail.com>
2020-03-18 19:57:02 +0100

    Header top and bottom work

--- header.php
+++ header.php
@@ -26,16 +26,12 @@
 
 	<header id="masthead" class="site-header">
 
-		<!-- <div class="flex-container"> -->
-
 			<div id="header-top" class="flex-container">
-
 				<div id="header-left" class="flex-container">
 					<div id="header-logo">
 						<a href="<?php echo get_home_url(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="logo"></a>
 					</div><!-- #header-logo -->
-				</div><!-- .header-left -->
-
+				</div><!-- #header-left -->
 				<div id="header-right" class="flex-container">
 					<nav id="site-navigation" class="main-navigation">
 						<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'wwc' ); ?></button>
@@ -46,16 +42,26 @@
 						) );
 						?>
 					</nav><!-- #site-navigation -->
-				</div><!-- .header-right -->
-
-			</div><!-- .header-top -->
+					<div id="header-social" class="flex-container">
+						<a href="#" target="blank" class="img-row"><img src="<?php echo get_template_directory_uri(); ?>/img/social-facebook.png" alt="facebook"></a>
+						<a href="#" target="blank" class="img-row"><img src="<?php echo get_template_directory_uri(); ?>/img/social-skype.png" alt="skype" target="blank"></a>
+						<a href="#" target="_blank" class="img-row"><img src="<?php echo get_template_directory_uri(); ?>/img/social-whatsapp.png" alt="whatsapp"></a>
+						<a href="#" target="blank" class="img-row"><img src="<?php echo get_template_directory_uri(); ?>/img/social-twitter.png" alt="twitter"></a>
+					</div><!-- #header-social -->
+				</div><!-- #header-right -->
+			</div><!-- #header-top -->
 
 			<div id="header-bottom" class="flex-container">
 
-			</div><!-- .header-bottom -->
+				<div id="header-call-to-action">
 
-		<!-- </div> -->
-		<!-- .flex-container -->
+					<h2>We can help you reach your full potential</h2>
+					<p>Just give us a call and we’ll do the rest.</p>
+					<p>START NOW</p>
+
+				</div><!-- #header-call-to-action -->
+
+			</div><!-- #header-bottom -->
 
 		<!-- <div class="site-branding">
 			<?php
